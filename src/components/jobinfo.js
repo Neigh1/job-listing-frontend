ea0ad638e3a2fcab407388c9ecaef4e0112b37ad
Bonolomp <168598147+Bonolomp@users.noreply.github.com>
2024-10-03 18:26:05 +0200

    Create jobinfo.js

---
+++ src/components/jobinfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function JobInfo() {
+  return (
+    <div>
+      <h2>Job Details</h2>
+      <p>Job Type: Full-time</p>
+      <p>Location: Remote</p>
+      <p>Salary: R60,000 - R80,000</p>
+    </div>
+  );
+}
+
+export default JobInfo;
